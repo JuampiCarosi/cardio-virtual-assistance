d3ac6e70997e3f53b48ff3640b8f308e79892f62
juampicarosi <juampicarosi@gmail.com>
2025-04-08 10:58:26 -0300

    add cicles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,94 @@
-import Link from "next/link";
-
-import { LatestPost } from "@/app/_components/post";
-import { api, HydrateClient } from "@/trpc/server";
-
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
+    <main>
+      <section className="relative h-[600px] bg-[url(/bg-main.png)] bg-cover bg-center bg-no-repeat">
+        <div className="absolute bottom-35 left-32 z-100 flex h-full items-end">
+          <div className="flex flex-col justify-center">
+            <h1 className="text-6xl font-semibold text-rose-950">
+              cardio virtual assistance
+            </h1>
+            <h2 className="text-4xl font-medium text-rose-900">
+              remote cardiology service
+            </h2>
+            <button className="mt-10 max-w-32 rounded-full border border-gray-300 bg-rose-900/10 py-1 text-2xl font-medium text-rose-800">
+              Contact
+            </button>
           </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
-
-          <LatestPost />
         </div>
-      </main>
-    </HydrateClient>
+      </section>
+      <Circles />
+    </main>
+  );
+}
+
+function Circles() {
+  return (
+    <svg
+      width="2503"
+      height="2546"
+      viewBox="0 0 2503 2546"
+      className="absolute -top-25 -left-[60%] z-10"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {/* Define the radial gradient */}
+      <defs>
+        <radialGradient id="backgroundGradient" cx="50%" cy="50%" r="50%">
+          <stop offset="0%" stopColor="white" stopOpacity="0.8" />
+          <stop offset="85%" stopColor="white" stopOpacity="0.9" />
+          <stop offset="100%" stopColor="white" stopOpacity="1" />
+        </radialGradient>
+      </defs>
+
+      {/* Background circle with gradient */}
+      <circle
+        cx="1255.5"
+        cy="1271.5"
+        r="950"
+        fill="url(#backgroundGradient)"
+        stroke="none"
+      />
+
+      {/* Stroke circles */}
+      <circle
+        cx="1255.5"
+        cy="1271.5"
+        r="950"
+        fill="none"
+        stroke="#D9D9D9"
+        strokeWidth="1"
+      />
+      <circle
+        cx="1255.5"
+        cy="1271.5"
+        r="573"
+        fill="none"
+        stroke="#D9D9D9"
+        strokeWidth="1"
+      />
+      <circle
+        cx="1255.5"
+        cy="1252.5"
+        r="271"
+        fill="none"
+        stroke="#D9D9D9"
+        strokeWidth="1"
+      />
+      <circle
+        cx="1013.5"
+        cy="1080.5"
+        r="214"
+        fill="none"
+        stroke="#D9D9D9"
+        strokeWidth="1"
+      />
+      <circle
+        cx="1013.5"
+        cy="1080.5"
+        r="153"
+        fill="none"
+        stroke="#D9D9D9"
+        strokeWidth="1"
+      />
+    </svg>
   );
 }
